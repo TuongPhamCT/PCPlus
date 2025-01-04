1c0f08767a0fda1dd29c7fd874b3e223c6e39a29
Phạm Thanh Tường <tuongpt12503@gmail.com>
2025-01-04 11:21:22 +0700

    notification

--- lib/views/notification.dart
+++ lib/views/notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pcplus/themes/text_decor.dart';
+import 'package:pcplus/views/notification/confirm.dart';
 import 'package:pcplus/views/widgets/bottom/bottom_bar_custom.dart';
 
 class NotificationScreen extends StatefulWidget {
@@ -12,9 +14,26 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Notification Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notification',
+          style: TextDecor.robo24Medi.copyWith(color: Colors.black),
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.5),
+        ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ConfirmNoti();
+          },
+        ),
       ),
       bottomNavigationBar: BottomBarCustom(currentIndex: 2),
     );
---
+++ lib/views/notification/confirm.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:pcplus/themes/palette/palette.dart';
+import 'package:pcplus/themes/text_decor.dart';
+
+class ConfirmNoti extends StatefulWidget {
+  const ConfirmNoti({super.key});
+
+  @override
+  State<ConfirmNoti> createState() => _ConfirmNotiState();
+}
+
+class _ConfirmNotiState extends State<ConfirmNoti> {
+  bool isViewed = false;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isViewed = true;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: isViewed ? Colors.white : Palette.backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+          border: const Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 1,
+            ),
+            top: BorderSide(
+              color: Colors.grey,
+              width: 1,
+            ),
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 50,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://cdn.tgdd.vn/Files/2022/01/30/1413644/cac-thuong-hieu-tai-nghe-tot-va-duoc-ua-chuong-nha.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const Gap(10),
+            Container(
+              width: size.width - 80,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Title: Đang vận chuyển',
+                    style: TextDecor.robo15Medi,
+                  ),
+                  const Gap(8),
+                  Text(
+                    'Description: Đơn hàng của bạn đang được vận chuyển viet noti khong can description o truoc dau',
+                    style: TextDecor.robo15,
+                  ),
+                  const Gap(8),
+                  Text(
+                    '10:00 20/10/2022',
+                    style: TextDecor.robo14.copyWith(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
