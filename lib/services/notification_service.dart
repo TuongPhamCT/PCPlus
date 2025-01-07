4639d615b30edd65307bf976fca4c09adfba78db
HuynhDangQuang <hdquang437@gmail.com>
2025-01-07 12:19:15 +0700

    Update notification

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -13,11 +13,37 @@ class NotificationService {
   // TODO: Thong bao duoc tao ra tu nguoi dung
 
   Future<void> createOrderingNotification(OrderModel order) async {
-    String customerName = user!.name!;
-
     NotificationModel notification = NotificationModel(
-      title: "Order từ ${customerName}",
-      content: "",
+      title: "Đơn hàng mới",
+      content: "${order.receiverName} đã đặt hàng từ bạn. Mã order: ${order.orderID!}",
+      isRead: false,
+      date: DateTime.now(),
+      productImage: order.itemModel!.image,
+    );
+    _notificationRepo.addNotificationToFirestore(
+        order.itemModel!.sellerID!,
+        notification
+    );
+  }
+
+  Future<void> createCancelOrderingNotification(OrderModel order, String reason) async {
+    NotificationModel notification = NotificationModel(
+      title: "Đơn hàng đã bị hủy",
+      content: "${order.receiverName} đã hủy đơn (Mã order: ${order.orderID!}) với lý do: $reason",
+      isRead: false,
+      date: DateTime.now(),
+      productImage: order.itemModel!.image,
+    );
+    _notificationRepo.addNotificationToFirestore(
+        order.itemModel!.sellerID!,
+        notification
+    );
+  }
+
+  Future<void> createReceivedOrderNotification(OrderModel order) async {
+    NotificationModel notification = NotificationModel(
+      title: "Khách hàng đã nhận đơn",
+      content: "${order.receiverName} đã nhận được đơn hàng. Mã order: ${order.orderID!}",
       isRead: false,
       date: DateTime.now(),
       productImage: order.itemModel!.image,
@@ -29,4 +55,46 @@ class NotificationService {
   }
 
   // TODO: Thong bao duoc tao ra tu shop
+
+  Future<void> createShopCancelOrderingNotification(OrderModel order, String reason) async {
+    NotificationModel notification = NotificationModel(
+      title: "Đơn hàng đã bị hủy",
+      content: "${order.shopName} đã hủy đơn (Mã order: ${order.orderID!}) với lý do: $reason",
+      isRead: false,
+      date: DateTime.now(),
+      productImage: order.itemModel!.image,
+    );
+    _notificationRepo.addNotificationToFirestore(
+        order.receiverID!,
+        notification
+    );
+  }
+
+  Future<void> createShopConfirmOrderNotification(OrderModel order) async {
+    NotificationModel notification = NotificationModel(
+      title: "Shop đã nhận đơn",
+      content: "${order.shopName} đã đồng ý đơn hàng của bạn. Mã order: ${order.orderID!}",
+      isRead: false,
+      date: DateTime.now(),
+      productImage: order.itemModel!.image,
+    );
+    _notificationRepo.addNotificationToFirestore(
+        order.receiverID!,
+        notification
+    );
+  }
+
+  Future<void> createShopSentOrderNotification(OrderModel order) async {
+    NotificationModel notification = NotificationModel(
+      title: "Đơn hàng đang được giao",
+      content: "${order.shopName} đã gửi đơn hàng đến bạn. Mã order: ${order.orderID!}",
+      isRead: false,
+      date: DateTime.now(),
+      productImage: order.itemModel!.image,
+    );
+    _notificationRepo.addNotificationToFirestore(
+        order.receiverID!,
+        notification
+    );
+  }
 }
